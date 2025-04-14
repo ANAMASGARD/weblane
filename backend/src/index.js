0f27bf953f3d03cb2343935e834af884be77ccd1
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-04-14 19:51:43 +0530

    Our Groq API is working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,34 @@
 require("dotenv").config();
 
-console.log(process.env.GROQ_API_KEY);
+
+
+const { Groq }  = require('groq-sdk');
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+
+});
+
+async function main() {
+  const chatCompletion = await groq.chat.completions.create({
+    "messages": [
+      {
+        "role": "user",
+        "content": "write a function in javascript that takes a string and returns the string reversed"
+      }
+    ],
+    "model": "qwen-2.5-coder-32b",
+    "temperature": 0.6,
+    "max_completion_tokens": 4096,
+    "top_p": 0.95,
+    "stream": true,
+    "stop": null
+  });
+
+  for await (const chunk of chatCompletion) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || '');
+  }
+}
+
+main();
+
